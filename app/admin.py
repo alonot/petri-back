6c030f430b1317c303c46bf2abdb44cec394a18e
Purusharth1 <127526760+Purusharth1@users.noreply.github.com>
2024-06-27 09:29:46 +0530

    Update admin.py
    Register three events in admin 
    Event
    EventTablePaid
    EventTableFree

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Profile
+from .models import Event,EventTableFree,EventTablePaid
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Event)
+admin.site.register(EventTablePaid)
+admin.site.register(EventTableFree)
