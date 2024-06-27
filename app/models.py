8918fe49cdb71bb6b6a82871cf6a504801c939cf
alonot <priyanshup27092004@gmail.com>
2024-06-27 10:38:41 +0530

    merging Zeeshan's work

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+
 class Institute(models.Model):
     institutionType = models.CharField(max_length=255, default="",null=True)
     instiName = models.CharField(unique=True, max_length=255)
@@ -28,4 +29,21 @@ class CAProfile(models.Model):
     CACode = models.CharField(max_length=8)
     registration = models.IntegerField(default=0)
 
+
+class EventTable(models.Model):
+    name = models.CharField(max_length=100)                 #Set the length for the name of the eventsss
+    fee = models.IntegerField()  
+    min_members = models.IntegerField(default=0)            #Random number for the min member 
+    max_members = models.IntegerField(default=1000)         #Maximum member for the event 
+
+    def __str__(self):
+        return self.name
+
+
+class Extras(models.Model):
+    extra_id = models.AutoField(primary_key=True)  # Here I have set this to the autofild mode whcih will  auto-generate primary key
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"Extra ID: {self.extra_id}, Price: {self.price}"
 
