c76d6753f1f094ba02758c0fb31d1588c113ac83
Chirag9528 <chiragvarshney9528@gmail.com>
2024-06-28 16:22:06 +0530

    forget password & change password done

--- app/models.py
+++ app/models.py
@@ -22,6 +22,7 @@ class Profile(models.Model):
     gradYear = models.IntegerField(default=6969)
     stream = models.TextField(null=True)
     joined = models.DateTimeField(auto_now_add=True)
+    forget_password_token = models.CharField(max_length=100 , default="")
 
     def __str__(self):
         return self.username
