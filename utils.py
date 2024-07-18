a80aeacd79d83a99f305b926c03305e86cccc17b
alonot <priyanshup27092004@gmail.com>
2024-07-18 21:27:14 +0530

    link change

--- utils.py
+++ utils.py
@@ -101,16 +101,7 @@ def method_not_allowed():
 
 def send_forget_password_mail(email , token):
     subject = 'Your forget password link'
-    message = f'Hi , Click on the link to reset your password http://127.0.0.1:8000/api/change-password/{token}/'
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = [email]
-    send_mail(subject , message , email_from , recipient_list)
-    return True
-
-
-def send_forget_password_mail(email , token):
-    subject = 'Your forget password link'
-    message = f'Hi , Click on the link to reset your password http://localhost:5173/change-password/{token}/'
+    message = f'Hi , Click on the link to reset your password  https://petrichor-events.vercel.app/change-password/{token}/'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject , message , email_from , recipient_list)
