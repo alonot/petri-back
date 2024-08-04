0f58c87a3599d40e2345cae6331b40740149df92
alonot <priyanshup27092004@gmail.com>
2024-08-04 15:52:39 +0530

    .vercel.app to allowedHost

--- petri_ca/settings.py
+++ petri_ca/settings.py
@@ -40,7 +40,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173', 'https://petrichor.events'
 ]
 ALLOWED_HOSTS = [
-    'localhost','127.0.0.1', 'https://petrichor.events', 'https://petrichor-events.vercel.app',"petri-back.vercel.app"
+    'localhost','127.0.0.1', 'https://petrichor.events', 'https://petrichor-events.vercel.app',"petri-back.vercel.app", ".vercel.app"
 ]
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173', 'https://petrichor.events'
