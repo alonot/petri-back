923e90bc2fe4e70aefb4a38d2f018d0dee4a8d52
alonot <priyanshup27092004@gmail.com>
2024-08-04 16:06:55 +0530

    added https://petrichor-events.vercel.app/ to CSRF_ALLOWED

--- petri_ca/settings.py
+++ petri_ca/settings.py
@@ -37,13 +37,13 @@ CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:5173', 'https://petrichor.events'
+    'http://localhost:5173', 'https://petrichor.events', 'https://petrichor-events.vercel.app/'
 ]
 ALLOWED_HOSTS = [
     'localhost','127.0.0.1', 'https://petrichor.events', 'https://petrichor-events.vercel.app',"petri-back.vercel.app", ".vercel.app"
 ]
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:5173', 'https://petrichor.events'
+    'http://localhost:5173', 'https://petrichor.events', 'https://petrichor-events.vercel.app/'
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # or 'django.contrib.sessions.backends.cache'
