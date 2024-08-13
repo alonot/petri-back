71629a3e08cfbc12e5abd9568c825294f9fcdfff
alonot <priyanshup27092004@gmail.com>
2024-08-13 16:38:36 +0530

    registring models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Profile
-from .models import Event,TransactionTable,Institute,CAProfile
+from .models import Event,TransactionTable,Institute,CAProfile,UserRegistrations, Extras
 
 # Register your models here.
 admin.site.register(Profile)
@@ -8,3 +8,5 @@ admin.site.register(Event)
 admin.site.register(TransactionTable)
 admin.site.register(Institute)
 admin.site.register(CAProfile)
+admin.site.register(UserRegistrations)
+admin.site.register(Extras)
