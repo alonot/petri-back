cb4e6b15db645640e3575da59d78e895cd8055aa
alonot <priyanshup27092004@gmail.com>
2024-09-10 11:36:07 +0530

    internal urls - / at end

--- internal/urls.py
+++ internal/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     # path('events/',views.getEventUsers ,name="event"),  # under progress
-    path('verifyTR',views.verifyTR,name="verifyTR"),  # under progress   get a list of TRs and verify them Zeeshan
-    path('unvertrid',views.unverifTR,name="unvertrid"), # get a list of TRs and unverify them Zeeshan
+    path('verifyTR/',views.verifyTR,name="verifyTR"),  # under progress   get a list of TRs and verify them Zeeshan
+    path('unvertrid/',views.unverifTR,name="unvertrid"), # get a list of TRs and unverify them Zeeshan
     # path('events/unconfirmed',views.getUnconfirmed ,name="unconfirmed"),  # new needed
     path('events/add/',views.addEvent,name="addEvent"),
     path('events/update/',views.updateEvent,name="updateEvent"),
