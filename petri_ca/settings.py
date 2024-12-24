00367066935a0ef70ec07f07942acd8e0e6de28f
PetrichorIITPkd <petrichor@iitpkd.ac.in>
2024-12-24 17:38:59 +0530

    netlify

--- petri_ca/settings.py
+++ petri_ca/settings.py
@@ -43,13 +43,13 @@ CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://petrichor.events', 'https://petrichor-events.vercel.app', 'https://x.petrichor.events',"https://finance-petrichor.vercel.app","https://petrichor-backend.vercel.app",
+    'https://petrichor.events', 'https://petrichor-events.vercel.app', 'https://x.petrichor.events',"https://finance-petrichor.vercel.app", "https://finance-petrichor.netlify.app","https://petrichor-backend.vercel.app",
 ]
 ALLOWED_HOSTS = [
-    'https://petrichor.events', 'https://petrichor-events.vercel.app', 'x.petrichor.events',"petrichor-backend.vercel.app", "petri-back.vercel.app","https://finance-petrichor.vercel.app","finance-petrichor.vercel.app",
+    'https://petrichor.events', 'https://petrichor-events.vercel.app', 'x.petrichor.events',"petrichor-backend.vercel.app", "petri-back.vercel.app","https://finance-petrichor.vercel.app","finance-petrichor.vercel.app", "https://finance-petrichor.netlify.app", "finance-petrichor.vercel.app"
 ]
 CORS_ORIGIN_WHITELIST = [
-     'https://petrichor.events', 'https://petrichor-events.vercel.app', 'https://.petrichor.events',"https://finance-petrichor.vercel.app","https://petrichor-backend.vercel.app", 
+     'https://petrichor.events', 'https://petrichor-events.vercel.app', 'https://.petrichor.events',"https://finance-petrichor.vercel.app", "https://finance-petrichor.netlify.app","https://petrichor-backend.vercel.app", 
 ]
 
 
